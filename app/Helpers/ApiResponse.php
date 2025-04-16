201f349dd9e1d4f55bc3fe2e0379d5bd43041596
Leonid <144706909+Lonja971@users.noreply.github.com>
2025-04-16 10:25:24 +0200

    Test commit 2

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -14,7 +14,6 @@ class ApiResponse
     * @param mixed $data Additional details for return.
     * @param int $statusCode HTTP status code.
     * @return JsonResponse
-    * test commit
     */
     public static function createResponseWithMessage(string $status, string $message, $data = null, int $statusCode = 200): JsonResponse
     {
